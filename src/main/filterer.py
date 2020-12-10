9dc16d37d893ea24bf3150ab338d91752ba08e41
Kari <k.sadat@gmail.com>
2020-12-09 20:27:05 -0500

    update code

--- src/main/filterer.py
+++ src/main/filterer.py
@@ -1,17 +1,21 @@
 # Created by Leon Hunter at 12:10 PM 12/08/2020
 class Filterer(object):
-    def remove_characters(string_to_remove_from, characters_to_remove):
+    def remove_characters(self, string_to_remove_from, characters_to_remove):
         result = str(string_to_remove_from)
-        for char in string_to_remove_from:
-            if char in characters_to_remove:
-                result = result.replace(char, '')
+        for removeable in characters_to_remove:
+            result = result.replace(removeable, '')
         return result
 
     def remove_vowels(self, string_to_remove_from):
-        vowels = list("aeiouAEIOU")
-        return self.remove_characters(string_to_remove_from, vowels)
+        lower_case_vowels = 'aeiou'
+        upper_case_vowels = lower_case_vowels.upper()
+        vowels = lower_case_vowels + upper_case_vowels
+        vowels_list = list(vowels)
+        return self.remove_characters(string_to_remove_from, vowels_list)
 
     def remove_consonants(self, string_to_remove_from):
-        consonant = "bcdfghjklmnpqrstvwxyz"
-        consonant_List= consonant.lower() + consonant.upper()
-        return self.remove_characters(string_to_remove_from,consonant_List)
+        lower_case_consonants = 'bcdfghjklmnpqrstvwxyz'
+        upper_case_consonants = lower_case_consonants.upper()
+        consonants = lower_case_consonants + upper_case_consonants
+        consonant_list = list(consonants)
+        return self.remove_characters(string_to_remove_from, consonant_list)
